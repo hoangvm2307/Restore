d1ff3d0d7e4ec296a223b4389b72c6721c1d6bc8
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2023-05-18 15:04:26 +0700

    End of 5

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -4,7 +4,8 @@ import Header from "./Header";
 import { ThemeProvider } from "@emotion/react";
 import { useState } from "react";
 import { Outlet } from "react-router-dom";
-
+import { ToastContainer } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 function App() {
   const[darkMode, setDarkMode] = useState(false);
   const paletteType = darkMode ? 'dark' : 'light';
@@ -22,6 +23,7 @@ function App() {
   }
   return (
     <ThemeProvider theme={theme}>
+      <ToastContainer position="bottom-right" hideProgressBar theme="colored"/>
       <CssBaseline />
       <Header darkMode={darkMode} handleThemeChange={handleThemeChange}/>
       <Container>
