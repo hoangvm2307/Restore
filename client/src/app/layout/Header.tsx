3aec4de6fc09c7a7e0d81ce7f16f05c97b5a1d03
Võ Minh Hoàng <vominhhoangkhung@gmail.com>
2023-05-19 13:19:55 +0700

    End of 6

--- client/src/app/layout/Header.tsx
+++ client/src/app/layout/Header.tsx
@@ -1,6 +1,7 @@
 import { ShoppingCart } from "@mui/icons-material";
 import { AppBar, IconButton, List, ListItem, Switch, Toolbar, Typography, Badge, Box } from "@mui/material";
-import { NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
+import { useStoreContext } from "../context/StoreContext";
 
 const midLinks = [
   {title: 'catalog', path: '/catalog'},
@@ -30,6 +31,9 @@ interface Props{
   handleThemeChange: () => void;
 }
 export default function Header({darkMode, handleThemeChange} : Props) {
+  const {basket} = useStoreContext();
+  const itemCount = basket?.items.reduce((sum,item)=> sum + item.quantity, 0);
+  
   return (
     <AppBar position="static" sx={{ mb: 4 }}>
       <Toolbar sx={{display: 'flex', justifyContent:'space-between', alignItems:'center'}}>
@@ -56,8 +60,8 @@ export default function Header({darkMode, handleThemeChange} : Props) {
         </List> 
 
         <Box display='flex' alignItems='center'>
-          <IconButton size='large' edge='start' color='inherit' sx={{mr:2}}>
-              <Badge badgeContent='4' color="secondary">
+          <IconButton component={Link} to='/basket' size='large' color='inherit' sx={{mr:2}}>
+              <Badge badgeContent={itemCount} color="secondary">
                 <ShoppingCart/>
               </Badge>
           </IconButton>
